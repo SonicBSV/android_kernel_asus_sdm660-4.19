ba1c140e52d13dfa598080c8af00479629fd8afd
Jesper Dangaard Brouer <brouer@redhat.com>
2019-02-19 19:53:02 +0100

    UPSTREAM: bpf: add skb->queue_mapping write access from tc clsact
    The skb->queue_mapping already have read access, via __sk_buff->queue_mapping.
    
    This patch allow BPF tc qdisc clsact write access to the queue_mapping via
    tc_cls_act_is_valid_access.  Also handle that the value NO_QUEUE_MAPPING
    is not allowed.
    
    It is already possible to change this via TC filter action skbedit
    tc-skbedit(8).  Due to the lack of TC examples, lets show one:
    
      # tc qdisc  add  dev ixgbe1 clsact
      # tc filter add  dev ixgbe1 ingress matchall action skbedit queue_mapping 5
      # tc filter list dev ixgbe1 ingress
    
    The most common mistake is that XPS (Transmit Packet Steering) takes
    precedence over setting skb->queue_mapping. XPS is configured per DEVICE
    via /sys/class/net/DEVICE/queues/tx-*/xps_cpus via a CPU hex mask. To
    disable set mask=00.
    
    The purpose of changing skb->queue_mapping is to influence the selection of
    the net_device "txq" (struct netdev_queue), which influence selection of
    the qdisc "root_lock" (via txq->qdisc->q.lock) and txq->_xmit_lock. When
    using the MQ qdisc the txq->qdisc points to different qdiscs and associated
    locks, and HARD_TX_LOCK (txq->_xmit_lock), allowing for CPU scalability.
    
    Due to lack of TC examples, lets show howto attach clsact BPF programs:
    
     # tc qdisc  add  dev ixgbe2 clsact
     # tc filter add  dev ixgbe2 egress bpf da obj XXX_kern.o sec tc_qmap2cpu
     # tc filter list dev ixgbe2 egress
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -6580,6 +6580,7 @@ static bool tc_cls_act_is_valid_access(int off, int size,
 		case bpf_ctx_range(struct __sk_buff, tc_classid):
 		case bpf_ctx_range_till(struct __sk_buff, cb[0], cb[4]):
 		case bpf_ctx_range(struct __sk_buff, tstamp):
+		case bpf_ctx_range(struct __sk_buff, queue_mapping):
 			break;
 		default:
 			return false;
