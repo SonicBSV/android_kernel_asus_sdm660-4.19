fc6a5d2a9db5d29d34d00479e0ba2c05d4525078
SonicBSV <sonicbsv@gmail.com>
2024-07-28 13:08:17 +0300

    fix build after upstream

--- init/initramfs.c
+++ init/initramfs.c
@@ -603,6 +603,17 @@ static void __init clean_rootfs(void)
 }
 #endif
 
+static int __initdata do_skip_initramfs;
+
+static int __init skip_initramfs_param(char *str)
+{
+	if (*str)
+		return 0;
+	do_skip_initramfs = 1;
+	return 1;
+}
+__setup("skip_initramfs", skip_initramfs_param);
+
 #ifdef CONFIG_BLK_DEV_RAM
 static void __init populate_initrd_image(char *err)
 {
