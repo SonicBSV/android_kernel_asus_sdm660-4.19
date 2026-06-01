b6a63a4e620bf8dfdb29137f7e03c8a6a4059235
SonicBSV <sonicbsv@ya.ru>
2026-06-01 16:36:16 +0300

    X00TD/X01BD: drivers: input: revert fp changes

--- drivers/input/fingerprint/focaltech_X01BD/ff_ctl.c
+++ drivers/input/fingerprint/focaltech_X01BD/ff_ctl.c
@@ -207,32 +207,28 @@ static void ff_ctl_device_event(struct work_struct *ws)
 {
     ff_ctl_context_t *ctx = container_of(ws, ff_ctl_context_t, work_queue);
     char *uevent_env[2] = {"FF_INTERRUPT", NULL};
+    FF_LOGV("'%s' enter.", __func__);
 
-    /* OPTIMIZATION: Продлеваем wakelock */
-    __pm_wakeup_event(ctx->wake_lock, 3000); /* 3 секунды */
-    
+    FF_LOGD("%s(irq = %d, ..) toggled.", __func__, ctx->irq_num);
+    __pm_wakeup_event(g_context->wake_lock, 2 * HZ); // 2 seconds.
     kobject_uevent_env(&ctx->miscdev.this_device->kobj, KOBJ_CHANGE, uevent_env);
+
+    FF_LOGV("'%s' leave.", __func__);
 }
 
 static irqreturn_t ff_ctl_device_irq(int irq, void *dev_id)
 {
     ff_ctl_context_t *ctx = (ff_ctl_context_t *)dev_id;
-
-    /* OPTIMIZATION: Мгновенный wakelock на 2 секунды */
-    if (ctx->wake_lock) {
-        __pm_wakeup_event(ctx->wake_lock, 2000);
-    }
-
-    /* OPTIMIZATION: Не отключаем IRQ - это добавляет задержку */
+    disable_irq_nosync(irq);
     if (likely(irq == ctx->irq_num)) {
-        if (g_config && g_config->enable_fasync && ctx->async_queue) {
-            kill_fasync(&ctx->async_queue, SIGIO, POLL_IN);
+        if (g_config && g_config->enable_fasync && g_context->async_queue) {
+            kill_fasync(&g_context->async_queue, SIGIO, POLL_IN);
         } else {
-            /* OPTIMIZATION: Используем schedule_work напрямую без disable/enable */
             schedule_work(&ctx->work_queue);
         }
     }
 
+    enable_irq(irq);
     return IRQ_HANDLED;
 }
 
--- drivers/input/fingerprint/goodix_X00TD/gf_spi.h
+++ drivers/input/fingerprint/goodix_X00TD/gf_spi.h
@@ -1,59 +1,55 @@
 /*
- * Optimized driver for Goodix fingerprint sensor
- * Copyright (C) 2016 Goodix
- * Optimized for ASUS X00TD kernel 4.19
+ * driver definition for sensor driver
+ *
+ * Coypright (c) 2017 Goodix
  */
 #ifndef __GF_SPI_H
 #define __GF_SPI_H
 
 #include <linux/types.h>
 #include <linux/notifier.h>
-#include <linux/spinlock.h>
-
-/* Mode definitions */
-enum FP_MODE {
-    GF_IMAGE_MODE = 0,
-    GF_KEY_MODE,
-    GF_SLEEP_MODE,
-    GF_FF_MODE,
-    GF_DEBUG_MODE = 0x56
+/**********************************************************/
+enum FP_MODE{
+	GF_IMAGE_MODE = 0,
+	GF_KEY_MODE,
+	GF_SLEEP_MODE,
+	GF_FF_MODE,
+	GF_DEBUG_MODE = 0x56
 };
 
-/* Key input mappings */
-#define GF_KEY_INPUT_HOME   KEY_HOME
-#define GF_KEY_INPUT_MENU   KEY_MENU
-#define GF_KEY_INPUT_BACK   KEY_BACK
-#define GF_KEY_INPUT_POWER  KEY_POWER
-#define GF_KEY_INPUT_CAMERA KEY_CAMERA
+#define GF_KEY_INPUT_HOME		KEY_HOME
+#define GF_KEY_INPUT_MENU		KEY_MENU
+#define GF_KEY_INPUT_BACK		KEY_BACK
+#define GF_KEY_INPUT_POWER		KEY_POWER
+#define GF_KEY_INPUT_CAMERA		KEY_CAMERA
 
 typedef enum gf_key_event {
-    GF_KEY_NONE = 0,
-    GF_KEY_HOME,
-    GF_KEY_POWER,
-    GF_KEY_MENU,
-    GF_KEY_BACK,
-    GF_KEY_CAMERA,
+	GF_KEY_NONE = 0,
+	GF_KEY_HOME,
+	GF_KEY_POWER,
+	GF_KEY_MENU,
+	GF_KEY_BACK,
+	GF_KEY_CAMERA,
 } gf_key_event_t;
 
 struct gf_key {
-    enum gf_key_event key;
-    uint32_t value;
+	enum gf_key_event key;
+	uint32_t value;   /* key down = 1, key up = 0 */
 };
 
 struct gf_key_map {
-    unsigned int type;
-    unsigned int code;
+	unsigned int type;
+	unsigned int code;
 };
 
 struct gf_ioc_chip_info {
-    unsigned char vendor_id;
-    unsigned char mode;
-    unsigned char operation;
-    unsigned char reserved[5];
+	unsigned char vendor_id;
+	unsigned char mode;
+	unsigned char operation;
+	unsigned char reserved[5];
 };
 
-/* IOCTL definitions */
-#define GF_IOC_MAGIC    'g'
+#define GF_IOC_MAGIC    'g'     //define magic number
 #define GF_IOC_INIT             _IOR(GF_IOC_MAGIC, 0, uint8_t)
 #define GF_IOC_EXIT             _IO(GF_IOC_MAGIC, 1)
 #define GF_IOC_RESET            _IO(GF_IOC_MAGIC, 2)
@@ -69,51 +65,54 @@ struct gf_ioc_chip_info {
 #define GF_IOC_REMOVE           _IO(GF_IOC_MAGIC, 12)
 #define GF_IOC_CHIP_INFO        _IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
 
-/* Configuration */
-#define USE_PLATFORM_BUS    1
-#define GF_NETLINK_ENABLE   1
-#define GF_NET_EVENT_IRQ        1
-#define GF_NET_EVENT_FB_BLACK   2
+//#define AP_CONTROL_CLK       1
+#define  USE_PLATFORM_BUS     1
+//#define  USE_SPI_BUS	1
+//#define GF_FASYNC   1	/*If support fasync mechanism.*/
+#define GF_NETLINK_ENABLE 1
+#define GF_NET_EVENT_IRQ 1
+#define GF_NET_EVENT_FB_BLACK 2
 #define GF_NET_EVENT_FB_UNBLACK 3
-#define NETLINK_TEST            25
-
-/* Timing constants */
-#define WAKELOCK_HOLD_TIME      400  /* ms */
-#define RESET_DELAY_US          2500 /* us */
-#define POWER_ON_DELAY_US       8000 /* us */
+#define NETLINK_TEST 25
 
 struct gf_dev {
-    dev_t devt;
-    struct list_head device_entry;
-    struct platform_device *spi;
-    struct input_dev *input;
+	dev_t devt;
+	struct list_head device_entry;
+#if defined(USE_SPI_BUS)
+	struct spi_device *spi;
+#elif defined(USE_PLATFORM_BUS)
+	struct platform_device *spi;
+#endif
+	struct clk *core_clk;
+	struct clk *iface_clk;
 
-    /* GPIO */
-    signed irq_gpio;
-    signed reset_gpio;
-    signed vdd_gpio;
-    int irq;
-
-    /* State tracking with spinlock for IRQ-safe access */
-    spinlock_t irq_lock;
-    unsigned users;
-    bool irq_enabled;
-    bool device_available;
-    bool fb_black;
-    bool irq_requested;
-
-    struct notifier_block notifier;
+	struct input_dev *input;
+	/* buffer is NULL unless this device is open (users > 0) */
+	unsigned users;
+	signed irq_gpio;
+	signed reset_gpio;
+	signed vdd_gpio;
+	int irq;
+	bool irq_enabled;
+	bool clk_enabled;
+#ifdef GF_FASYNC
+	struct fasync_struct *async;
+#endif
+	struct notifier_block notifier;
+	bool device_available;
+	bool fb_black;
 };
 
-/* Function prototypes */
-int gf_parse_dts(struct gf_dev *gf_dev);
+int gf_parse_dts(struct gf_dev* gf_dev);
 void gf_cleanup(struct gf_dev *gf_dev);
+
 int gf_power_on(struct gf_dev *gf_dev);
 int gf_power_off(struct gf_dev *gf_dev);
+
 int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms);
 int gf_irq_num(struct gf_dev *gf_dev);
+
 void sendnlmsg(char *message);
 int netlink_init(void);
 void netlink_exit(void);
-
-#endif /* __GF_SPI_H */
+#endif /*__GF_SPI_H*/
